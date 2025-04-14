602c8cc1c12307dbec8782f6656b5b1121aae2fc
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-04-15 02:16:30 +1000

    update build

--- flake.nix
+++ flake.nix
@@ -3,42 +3,68 @@
 
   inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { self, utils, ... }: {
-    overlays.default = final: prev: {
-      translate = self.packages.${prev.system}.default;
-    };
-    packages = utils.lib.eachSystem { } (pkgs: {
-      default = pkgs.rustPlatform.buildRustPackage {
-        name = "translate";
-        pname = "tl";
-        version = "1.0.0";
-        src = ./.;
-        useFetchCargoVendor = true;
-        cargoHash = "sha256-xMA2bus+ElNQhc9E7aK7XC1Zfuu8p0TW9fkWfv5z2AA=";
-        buildInputs = with pkgs;
-          [ libiconv ] ++ lib.optional stdenv.hostPlatform.isDarwin
-          darwin.apple_sdk.frameworks.SystemConfiguration;
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out/bin $out/include $out/lib
-          # Dynamically find header bin and lib files
-          cp $(find target -type f \( -name 'translate' -o -name 'translate.exe' \) | head -n1) $out/bin
-          cp $(find target -name translate.h | head -n1) $out/include
-          cp $(find target -name 'libtranslate\.*' ! -name 'libtranslate.d' | head -n1) $out/lib
-          runHook postInstall
-        '';
-        meta = {
-          description = "A command-line translator powered by Google Translate";
-          longDescription = ''
-            This command-line tool leverages Google Translate to translate text quickly between languages.
-            It can be used to translate command-line output, allowing users to work with any language from the terminal.
-          '';
-          homepage = "https://github.com/NewDawn0/tl";
-          license = pkgs.lib.licenses.mit;
-          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-          platforms = pkgs.lib.platforms.all;
+  outputs = { self, utils, ... }:
+    let
+      hash = "sha256-sKm+AU0TMXBm/OrOYKM6hZVRfJcEqP27O+6WNMoZxSg=";
+      toml = with builtins; (fromTOML (readFile ./Cargo.toml));
+      package = pkgs: self.packages.${pkgs.system}.default;
+      inherit (toml.package) name version;
+      src = ./.;
+    in {
+      overlays.default = final: prev: { translate = package prev; };
+      devShells = utils.lib.eachSystem { } (pkgs: {
+        default = pkgs.mkShell {
+          inherit name;
+          buildInputs = [ (package pkgs) ];
+          CPATH = "${package pkgs}/include";
+          LIBRARY_PATH = "${package pkgs}/lib";
+          DYLD_LIBRARY_PATH = "${package pkgs}/lib";
         };
-      };
-    });
-  };
+      });
+      packages = utils.lib.eachSystem { } (pkgs:
+        let
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            inherit hash src;
+            useFetchCargoVendor = true;
+          };
+          cargoConfig = (pkgs.formats.toml { }).generate "config.toml" {
+            source = {
+              crates-io.replace-with = "vendored-sources";
+              vendored-sources.directory = cargoDeps;
+            };
+          };
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            inherit name version src;
+            buildInputs = with pkgs;
+              [ cargo ] ++ lib.optionals pkgs.stdenv.isDarwin [ libiconv ];
+            configurePhase = ''
+              mkdir -p .cargo
+              install ${cargoConfig} .cargo/config.toml
+            '';
+            buildPhase = ''
+              CARGO_HOME=$(pwd)
+              cargo build --release --offline
+            '';
+            installPhase = ''
+              mkdir -p $out/lib $out/bin
+              install -D target/release/translate $out/bin/translate
+              mv target/release/libtranslate.* $out/lib/
+              mv target/include $out/
+              rm -f $out/lib/libtranslate.d
+            '';
+            meta = {
+              description =
+                "CLI translator and dynamic library using Google Translate";
+              longDescription = ''
+                A fast command-line translator with a C-compatible dynamic library (`libtranslate`) and header (`libtranslate.h`), enabling easy integration into C/C++ projects.
+              '';
+              homepage = "https://github.com/NewDawn0/translate";
+              license = pkgs.lib.licenses.mit;
+              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+              platforms = pkgs.lib.platforms.all;
+            };
+          };
+        });
+    };
 }
